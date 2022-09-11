27c6aa6c3b8653b5ce7d31266fffe7f7ae577b38
Chifaith <chidinmaaforkah@gmail.com>
2022-09-11 12:04:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
-/*
- *main - Entry point
- *takes no arguments
- *
- *Description: contains a puts() function to print string to standard output
- *Return: function returns 0
+/**
+ * main - program starts and ends in this function
+ * takes no arguments
+ * Description: contains a puts() function to print string to standard output
+ * Return: function returns 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
