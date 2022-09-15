6cba5908b6f6bd87a0d2c684c2e20d1f38bddc52
Chifaith <chidinmaaforkah@gmail.com>
2022-09-14 20:27:43 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints lowercase alphabet
+ * Takes no argument values.
+ *
+ * Return: no specified return value.
+ */
+void print_alphabet(void)
+{
+	int ch = 97;
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar(10);
+}
