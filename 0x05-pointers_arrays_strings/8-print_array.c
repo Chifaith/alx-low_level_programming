0dbf6e9c84949e7a263e5b37ab422f52c38bf0ad
Chifaith <chidinmaaforkah@gmail.com>
2022-09-20 18:41:24 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,31 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_array - Prints n elements of an array of integers.
- * @a: Pointer to to array.
- * @n: Number of elements.
+ * print_array - prints array
+ * @a: pointer of array
+ * @n: how many times to prints
  *
- * Return: No specified return value.
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (i == n - 1)
-			{
-				printf("%d\n", a[i]);
-				break;
-			}
-			printf("%d, ", a[i]);
-		}
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
 	}
-	else
-	{
-		printf("\n");
-	}
+	printf("\n");
 }
